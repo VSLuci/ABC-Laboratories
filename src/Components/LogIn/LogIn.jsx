e1d48a6689db8b1d685db2ba73429bf5e587238a
Vimukthi Shohan <vimukthi.shohan@gmail.com>
2020-09-05 23:27:55 +0530

    haduva

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,209 +1,75 @@
 import React, { Component } from 'react';
-import GoogleFontLoader from 'react-google-font-loader';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
-import "../css/common.css";
-import { Form, Button, InputGroup } from 'react-bootstrap'
-import { login } from "../../API/authAPI";
+import { Form, Input, Button, Checkbox } from 'antd';
+import '../css/common.css';
+import { login } from '../../API/authAPI';
 
 class LogIn extends Component {
-
-  onSubmit = async ({ email, password }) => {
-    const { history } = this.props;
-
-    await login({ email, password, history });
-  }
-
+  onFinish = async values => {
+    const history = this.props.history;
+    return await login({ email: values.email, password: values.password, history });
+  };
+  onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+  };
   render() {
+    const layout = {
+      labelCol: { span: 8 },
+      wrapperCol: { span: 16 },
+    };
+    const tailLayout = {
+      wrapperCol: { offset: 8, span: 16 },
+    };
+
     return (
-      <div className="container-full">
-        <div className="bg-circle-1"></div>
-        <div className="bg-circle-2"></div>
-        <div className="bg-circle-3"></div>
-        <div className="bg-circle-4"></div>
-
-        {/* Google font loader applying */}
-        <GoogleFontLoader
-          fonts={[
-            {
-              font: 'Playfair Display',
-              weights: [500, 700],
-            },
-            {
-              font: 'Abril Fatface',
-              weights: [400, 700],
-            },
-            {
-              font: 'Abril Fatface',
-              weights: [400, 700],
-            },
-          ]}
-          subsets={['cyrillic-ext', 'greek']}
-        />
-
-        <div className="container col-6 fixed-top shadow mt-5 h-75 mb-4 bg-white">
-          <h1 className="mt-4 ml-4"
-            style={{ fontFamily: 'Playfair Display, serif' }}
+      <div>
+        <Form
+          {...layout}
+          name="basic"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={this.onFinish}
+          onFinishFailed={this.onFinishFailed}
+        >
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your username!',
+              },
+            ]}
           >
-            Login
-          </h1>
+            <Input />
+          </Form.Item>
 
-          <Form
-            className="text-center"
-            onSubmit={this.onSubmit}
-            method='post'
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
           >
+            <Input.Password />
+          </Form.Item>
 
-            <InputGroup className="mt-5 mb-4">
-              <InputGroup.Prepend>
-                <span className="input-group-text">
-                  <FontAwesomeIcon icon={faEnvelope} />
-                </span>
-              </InputGroup.Prepend>
-              <Form.Control
-                style={{ fontFamily: 'Playfair Display, serif' }}
-                className="form-control"
-                type="email"
-                placeholder="Email address"
-                required
-              />
-            </InputGroup>
+          <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
 
-            <InputGroup className="mb-4">
-              <InputGroup.Prepend>
-                <span className="input-group-text">
-                  <FontAwesomeIcon icon={faLock} />
-                </span>
-              </InputGroup.Prepend>
-              <Form.Control
-                style={{ fontFamily: 'Playfair Display, serif' }}
-                className="form-control"
-                type="password"
-                placeholder="Password"
-                required
-              />
-            </InputGroup>
-
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check
-                style={{ fontFamily: 'Eagle Lake, serif' }}
-                type="checkbox"
-                label="Stay signed in"
-              />
-            </Form.Group>
-
-            <Button
-              variant="btn btn-secondary mt-5 btn-lg btn-block btn-login"
-              style={{ fontFamily: 'Abril Fatface, cursive' }}
-              type="submit"
-            >
+          <Form.Item {...tailLayout}>
+            <Button type="primary" htmlType="submit">
               Submit
             </Button>
-
-          </Form>
-
-          <Form method='post'>
-
-            <Button
-              variant="outline-info btn-register mt-5 btn-lg btn-block"
-              style={{ fontFamily: 'Abril Fatface, cursive' }}
-              type="submit"
-            >
-              Create an account
-            </Button>
-
-          </Form>
-
-        </div>
+          </Form.Item>
+        </Form>
       </div>
     );
   }
 }
-
-// function LogIn(props) {
-
-//   return (
-//     <div className="container-full">
-//       <div className="bg-circle-1"></div>
-//       <div className="bg-circle-2"></div>
-//       <div className="bg-circle-3"></div>
-//       <div className="bg-circle-4"></div>
-
-//       {/* Google font loader applying */console.log(props)}
-//       <GoogleFontLoader
-//         fonts={[
-//           {
-//             font: 'Playfair Display',
-//             weights: [500, 700],
-//           },
-//           {
-//             font: 'Abril Fatface',
-//             weights: [400, 700],
-//           },
-//           {
-//             font: 'Abril Fatface',
-//             weights: [400, 700],
-//           },
-//         ]}
-//         subsets={['cyrillic-ext', 'greek']}
-//       />
-
-//       <div className="container col-6 fixed-top shadow mt-5 h-75 mb-4 bg-white">
-//         <h1 className="mt-4 ml-4"
-//           style={{ fontFamily: 'Playfair Display, serif' }}
-//         >
-//           Login
-//         </h1>
-
-//         <Form className="text-center" action='' method='post'>
-
-//           <InputGroup className="mt-5 mb-4">
-//             <InputGroup.Prepend>
-//               <span className="input-group-text">
-//                 <FontAwesomeIcon icon="envelope" />
-//               </span>
-//             </InputGroup.Prepend>
-//             <Form.Control
-//               style={{ fontFamily: 'Playfair Display, serif' }}
-//               type="email"
-//               placeholder="Email address"
-//             />
-//           </InputGroup>
-
-//           <InputGroup className="mb-4">
-//             <InputGroup.Prepend>
-//               <span className="input-group-text">
-//                 <FontAwesomeIcon icon="lock" />
-//               </span>
-//             </InputGroup.Prepend>
-//             <Form.Control
-//               style={{ fontFamily: 'Playfair Display, serif' }}
-//               type="password"
-//               placeholder="Password"
-//             />
-//           </InputGroup>
-
-//           <Form.Group controlId="formBasicCheckbox">
-//             <Form.Check
-//               style={{ fontFamily: 'Eagle Lake, serif' }}
-//               type="checkbox"
-//               label="Stay signed in"
-//             />
-//           </Form.Group>
-
-//           <Button
-//             variant="btn btn-secondary mt-5 btn-lg btn-block btn-login"
-//             style={{ fontFamily: 'Abril Fatface, cursive' }}
-//             type="submit"
-//           >
-//             Submit
-//           </Button>
-
-//         </Form>
-
-//       </div>
-//     </div>
-//   );
-// }
 
 export default LogIn;
