2cf97839eb7b668dfc0edf95996a62c133972715
VSLuci <vimukthi.shohan@gmail.com>
2020-09-13 08:45:25 +0530

    Reapplying interface

--- src/Components/LogIn/LogIn.jsx
+++ src/Components/LogIn/LogIn.jsx
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { Form, Input, Button, Checkbox } from 'antd';
+import GoogleFontLoader from 'react-google-font-loader';
+import "../FontAwesome/FontAwesomeIcons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 import '../css/common.css';
+import { InputGroup } from 'react-bootstrap'
 import { login } from '../../API/authAPI';
 
 class LogIn extends Component {
@@ -21,53 +26,122 @@ class LogIn extends Component {
     };
 
     return (
-      <div>
-        <Form
-          {...layout}
-          name="basic"
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={this.onFinish}
-          onFinishFailed={this.onFinishFailed}
-        >
-          <Form.Item
-            label="Email"
-            name="email"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your username!',
-              },
-            ]}
+      // <div>
+      <div className="container-full">
+        <div className="bg-circle-1"></div>
+        <div className="bg-circle-2"></div>
+        <div className="bg-circle-3"></div>
+        <div className="bg-circle-4"></div>
+        <div className="container col-6 fixed-top shadow mt-5 h-75 mb-4 bg-white">
+          <h1 className="mt-4 ml-4">Login</h1>
+          {/* <Form
+            {...layout}
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={this.onFinish}
+            onFinishFailed={this.onFinishFailed}
           >
-            <Input />
-          </Form.Item>
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-            ]}
-          >
-            <Input.Password />
-          </Form.Item>
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
 
-          <Form.Item {...tailLayout} name="remember" valuePropName="checked">
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
+            <Form.Item {...tailLayout}
+              name="remember"
+              valuePropName="checked"
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
 
-          <Form.Item {...tailLayout}>
-            <Button type="primary" htmlType="submit">
-              Submit
+            <Form.Item {...tailLayout}>
+              <Button
+                className="btn btn-secondary font-weight-bold btn-login"
+                type="primary"
+                htmlType="submit"
+              >
+                Sign in
             </Button>
-          </Form.Item>
-        </Form>
-      </div>
+            </Form.Item>
+          </Form> */}
+          <Form
+            {...layout}
+            name="basic"
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={this.onFinish}
+            onFinishFailed={this.onFinishFailed}
+          >
+            {/* <InputGroup> */}
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your username!',
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+            {/* </InputGroup> */}
+
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please input your password!',
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item {...tailLayout}
+              name="remember"
+              valuePropName="checked"
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item {...tailLayout}>
+              <Button
+                className="btn btn-secondary font-weight-bold btn-login"
+                type="primary"
+                htmlType="submit"
+              >
+                Sign in
+            </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div >
     );
   }
 }
